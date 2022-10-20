65787c60233e828c546a21285f115794d90b8f8c
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-10-20 21:52:43 +0530

    Add  CustomerProperty js setCustomProperty

--- updateCustomProperty.js
+++ updateCustomProperty.js
@@ -2,6 +2,8 @@ export function getCustomProperty(elem, prop) {
     return parseFloat(getComputedStyle(elem).getPropertyValue(prop)) || 0
 }
 
-
+export function setCustomProperty(elem, prop, value) {
+    elem.style.setProperty(prop, value)
+}
 
 
