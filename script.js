191e9d45064e774e30f936405b204feb4130ec62
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-10-20 21:45:21 +0530

    Add isCollision content

--- script.js
+++ script.js
@@ -40,7 +40,14 @@ function checkLose() {
     return getCactusRects().some(rect => isCollision(rect, dinoRect))
 }
 
-
+function isCollision(rect1, rect2) {
+    return (
+        rect1.left < rect2.right &&
+        rect1.top < rect2.bottom &&
+        rect1.right > rect2.left &&
+        rect1.bottom > rect2.top
+    )
+}
 
 
 
