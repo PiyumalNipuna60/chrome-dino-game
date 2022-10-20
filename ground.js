6b7dabd3b650af0e90dd38a3b3eb166383eef71d
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-10-20 21:50:49 +0530

    Add  ground js updateGround content

--- ground.js
+++ ground.js
@@ -8,4 +8,12 @@ export function setupGround() {
     setCustomProperty(groundElems[1], "--left", 300)
 }
 
+export function updateGround(delta, speedScale) {
+    groundElems.forEach(ground => {
+        incrementCustomProperty(ground, "--left", delta * speedScale * SPEED * -1)
 
+        if (getCustomProperty(ground, "--left") <= -300) {
+            incrementCustomProperty(ground, "--left", 600)
+        }
+    })
+}
