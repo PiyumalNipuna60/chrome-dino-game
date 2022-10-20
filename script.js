b817f5a1ab01f2600431fd7e43df9ceb1b694c9a
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-10-20 21:44:40 +0530

    Add Update content

--- script.js
+++ script.js
@@ -1,9 +1,51 @@
+
+
 const WORLD_WIDTH = 100
 const WORLD_HEIGHT = 30
-const worldElement = document.querySelector('[data-world]')
+const SPEED_SCALE_INCREASE = 0.00001
+
+const worldElem = document.querySelector("[data-world]")
+const scoreElem = document.querySelector("[data-score]")
+const startScreenElem = document.querySelector("[data-start-screen]")
 
 setPixelToWorldScale()
 window.addEventListener("resize", setPixelToWorldScale)
+document.addEventListener("keydown", handleStart, { once: true })
+
+let lastTime
+let speedScale
+let score
+
+function update(time) {
+    if (lastTime == null) {
+        lastTime = time
+        window.requestAnimationFrame(update)
+        return
+    }
+    const delta = time - lastTime
+
+    updateGround(delta, speedScale)
+    updateDino(delta, speedScale)
+    updateCactus(delta, speedScale)
+    updateSpeedScale(delta)
+    updateScore(delta)
+    if (checkLose()) return handleLose()
+
+    lastTime = time
+    window.requestAnimationFrame(update)
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 function setPixelToWorldScale() {
     let worldToPixelScale
@@ -12,8 +54,7 @@ function setPixelToWorldScale() {
     } else {
         worldToPixelScale = window.innerHeight / WORLD_HEIGHT
     }
-    worldElement.style.width = `${WORLD_WIDTH * worldToPixelScale}px`
-    worldElement.style.height = `${WORLD_HEIGHT * worldToPixelScale}px`
+
+    worldElem.style.width = `${WORLD_WIDTH * worldToPixelScale}px`
+    worldElem.style.height = `${WORLD_HEIGHT * worldToPixelScale}px`
 }
-
-
