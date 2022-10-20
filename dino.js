9b01b9cebb354886d4c449f9a8c9b24923885434
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-10-20 21:57:09 +0530

    Add  Dino js content

--- dino.js
+++ dino.js
@@ -4,7 +4,16 @@ import {
     getCustomProperty,
 } from "./updateCustomProperty.js"
 
+const dinoElem = document.querySelector("[data-dino]")
+const JUMP_SPEED = 0.45
+const GRAVITY = 0.0015
+const DINO_FRAME_COUNT = 2
+const FRAME_TIME = 100
 
+let isJumping
+let dinoFrame
+let currentFrameTime
+let yVelocity
 
 
 
