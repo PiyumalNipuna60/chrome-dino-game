f695103bfbfc1bcaae9c4f589de9370b4cc47327
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-10-21 21:01:17 +0530

    get variable'

--- cactus.js
+++ cactus.js
@@ -4,3 +4,14 @@ import {
     getCustomProperty,
 } from "./updateCustomProperty.js"
 
+const SPEED = 0.05
+const CACTUS_INTERVAL_MIN = 500
+const CACTUS_INTERVAL_MAX = 2000
+const worldElem = document.querySelector("[data-world]")
+
+
+
+
+
+
+
