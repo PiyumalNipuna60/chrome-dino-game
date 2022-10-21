04b13c83159d88d55b57d013c69267728e2cdf04
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-10-21 21:01:35 +0530

    setupCactus funtion

--- cactus.js
+++ cactus.js
@@ -9,7 +9,13 @@ const CACTUS_INTERVAL_MIN = 500
 const CACTUS_INTERVAL_MAX = 2000
 const worldElem = document.querySelector("[data-world]")
 
-
+let nextCactusTime
+export function setupCactus() {
+    nextCactusTime = CACTUS_INTERVAL_MIN
+    document.querySelectorAll("[data-cactus]").forEach(cactus => {
+        cactus.remove()
+    })
+}
 
 
 
