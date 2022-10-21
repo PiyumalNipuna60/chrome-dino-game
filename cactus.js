c9a9eb66aed73b241c4ee29a8306efaad782f683
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-10-21 21:04:37 +0530

    import js file

--- cactus.js
+++ cactus.js
@@ -42,7 +42,7 @@ export function getCactusRects() {
 function createCactus() {
     const cactus = document.createElement("img")
     cactus.dataset.cactus = true
-    cactus.src = "imgs/cactus.png"
+    cactus.src = "assets/cactus.png"
     cactus.classList.add("cactus")
     setCustomProperty(cactus, "--left", 100)
     worldElem.append(cactus)
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 import { updateGround, setupGround } from "./ground.js"
 import { updateDino, setupDino, getDinoRect, setDinoLose } from "./dino.js"
-
+import { updateCactus, setupCactus, getCactusRects } from "./cactus.js"
 
 const WORLD_WIDTH = 100
 const WORLD_HEIGHT = 30
